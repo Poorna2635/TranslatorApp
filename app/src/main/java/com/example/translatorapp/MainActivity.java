c3ad59c62549e7d41352d4bbd5f5f60c5ddc3b13
poorna2635 <gantapoornaramachandra@gmail.com>
2025-11-29 18:03:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/translatorapp/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.translatorapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.mlkit.common.model.DownloadConditions;
+import com.google.mlkit.nl.translate.TranslateLanguage;
+import com.google.mlkit.nl.translate.Translation;
+import com.google.mlkit.nl.translate.Translator;
+import com.google.mlkit.nl.translate.TranslatorOptions;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Translator translator;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText inputEditText = findViewById(R.id.inputEditText);
+        Button translateButton = findViewById(R.id.translateButton);
+        TextView outputText = findViewById(R.id.outputText);
+
+        TranslatorOptions options = new TranslatorOptions.Builder()
+                .setSourceLanguage(TranslateLanguage.ENGLISH)
+                .setTargetLanguage(TranslateLanguage.TELUGU)
+                .build();
+
+        translator = Translation.getClient(options);
+
+        DownloadConditions conditions = new DownloadConditions.Builder()
+                .requireWifi()
+                .build();
+
+        translator.downloadModelIfNeeded(conditions)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void v) {
+                        translateButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                String textToTranslate = inputEditText.getText().toString();
+                                translateText(textToTranslate, outputText);
+                            }
+                        });
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        outputText.setText("Model download failed: " + e.getMessage());
+                    }
+                });
+    }
+
+    // ✅ Moved outside onCreate()
+    private void translateText(String inputText, TextView outputTextView) {
+        translator.translate(inputText)
+                .addOnSuccessListener(new OnSuccessListener<String>() {
+                    @Override
+                    public void onSuccess(@NonNull String translatedText) {
+                        outputTextView.setText(translatedText);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        outputTextView.setText("Translation failed: " + e.getMessage());
+                    }
+                });
+    }
+}
